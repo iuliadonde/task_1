b2dc439ccf62fc374dfd808212b8f9d6033d6a96
comp14 <comp14>
2023-11-24 14:39:58 +0300

    test

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 a=[]
-with open('example_100kb.csv') as f:
-    for line in f:
-        a.append(line)
-print(a)
+with open('out.csv') as f:
+    for line in f.readlines():
+        s=line.strip().split(',')
+        a.append(s[2])
+
+k=[]
+for i in a:
+    k.append(i.split('@')[-1])
+
+d={}
+c=0
+for j in k:
+    if j not in d:
+        d[j] = 1
+    else:
+        d[j]+=1
+print(d.items())
